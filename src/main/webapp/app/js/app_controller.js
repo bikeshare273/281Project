e46e9bc07f849f91428f6f8a780268cc71356967
swapniljoshi15 <swapnil.joshi1590@gmail.com>
2015-04-25 15:58:58 -0700

    Added login and register project

--- src/main/webapp/app/js/app_controller.js
+++ src/main/webapp/app/js/app_controller.js
@@ -1,29 +1,54 @@
-var movieapp = angular.module('movieapp', [ 'ngRoute', 'ngResource']);
+var app = angular.module('app', [ 'ngRoute', 'ngResource', 'smart-table' ]);
 
-movieapp.run(function($rootScope) {
-    $rootScope.OffNavTabs = true;
-    $rootScope.OffLogoutTabs = true;
+app.run(function($rootScope) {
+	$rootScope.hideUserNavTabs = true;
+	$rootScope.hideStaticTabs = false;
 });
 
 //factory for sharing data between controller
-movieapp.factory('dataSharing', function() {
-	 var sharedData = {}
-	 function set(data) {
-		 sharedData = data;
-	 }
-	 function get() {
-		 return sharedData;
-	 }
+app.factory('dataSharing', function() {
+	var sharedData = {}
+	function set(data) {
+		sharedData = data;
+	}
+	function get() {
+		return sharedData;
+	}
 
-	 return {
-	  set: set,
-	  get: get
-	 }
+	return {
+		set : set,
+		get : get
+	}
 
 });
 
+app.directive('passwordMatch', [ function() {
+	return {
+		restrict : 'A',
+		scope : true,
+		require : 'ngModel',
+		link : function(scope, elem, attrs, control) {
+			var checker = function() {
+
+				//get the value of the first password
+				var e1 = scope.$eval(attrs.ngModel);
+
+				//get the value of the other password  
+				var e2 = scope.$eval(attrs.passwordMatch);
+				return e1 == e2;
+			};
+			scope.$watch(checker, function(n) {
+
+				//set the form control to valid if both 
+				//passwords are the same, else invalid
+				control.$setValidity("unique", n);
+			});
+		}
+	};
+} ]);
+
 // configure our routes
-movieapp.config(function($routeProvider) {
+app.config(function($routeProvider) {
 	$routeProvider
 
 	// route for the welcome page
@@ -31,41 +56,74 @@ movieapp.config(function($routeProvider) {
 		templateUrl : 'home.html',
 		controller : 'homeController'
 	})
-	
+
+	.when('/register', {
+		templateUrl : 'register.html',
+		controller : 'registerController'
+	})
+
+	.when('/home', {
+		templateUrl : 'HomeUser.html',
+		controller : 'homeUserController'
+	})
+
 	.otherwise({
 		redirectTo : '/'
 	});
 });
 
-movieapp.controller('homeController',
-	 				function($scope, $http, $location, $q, dataSharing, $timeout, $rootScope) {
+app.controller('homeController', function($scope, $http, $location, $q,
+		dataSharing, $timeout, $rootScope) {
 	console.log('homeController start');
-	
-	$scope.submitWelcomeMessage = function(){
-		console.log("username "+$scope.username);
-		//submit welcome message
-		var response = $http.get("../api/v1/demo/"+$scope.username);
-		console.log(response);
-		response.success(function(dataFromServer, status,
-						headers, config) {
-			console.log("dataFromServer" + dataFromServer);
-					if (dataFromServer) {
-						//validated go ahead
-						$scope.welcomemessage = dataFromServer.message;
-					}else{
-						$location.url('/');
-					}
-				});
-		response.error(function(data, status, headers, config) {
-			if (response.status === 401
-					|| response.status === 400) {
-				$scope.loginform_error = "Invalid request";
-				$location.url('/');
-				return $q.reject(response);
-			}
-		});
-	}
+	$rootScope.hideUserNavTabs = true;
+	$rootScope.hideStaticTabs = false;
 
+	$scope.loginform_login = function(item, event) {
+		console.log("--> Submitting form " + $scope.loginform_email + " "
+				+ $scope.loginform_password);
+		console.log("--> Submitting form ");
+		
+		$location.url('/home');
+	};
+
+	$scope.clickRegister = function() {
+		$location.url('/register');
+	}
 
 	console.log('homeController end');
 });
+
+app.controller('registerController',
+		function($scope, $http, $location, $q, dataSharing, $timeout, $rootScope) {
+	console.log('registerController start');
+	$rootScope.hideUserNavTabs = true;
+	$rootScope.hideStaticTabs = false;
+	
+	$scope.signupform_signup = function(item, event) {
+		console.log("--> Submitting form "
+				+ $scope.signupform_name + " "
+				+ $scope.signupform_email );
+		console.log("--> Submitting form "
+				+ $scope.signupform_phone + " "
+				+ $scope.signupform_address);
+		console.log("--> Submitting form "
+				+ $scope.signupform_city + " "
+				+ $scope.signupform_state+" "
+				+ $scope.signupform_country);
+		console.log("--> Submitting form ");
+		
+	};
+	console.log('registerController end');
+});
+
+app.controller('homeUserController',
+		function($scope, $http, $location, $q, dataSharing, $timeout, $rootScope) {
+	console.log('homeUserController start');
+	$rootScope.hideUserNavTabs = false;
+	$rootScope.hideStaticTabs = true;
+	
+	
+	console.log('homeUserController end');
+});
+
+
