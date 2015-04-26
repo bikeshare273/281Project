3f222a07d8927bc922d2a2f67868538e4e480c82
vireshjivane <viresh.jivane@sjsu.edu>
2015-04-26 03:22:12 -0700

    updated Field and Tenant Entities.

--- src/main/java/com/project/entities/Field.java
+++ src/main/java/com/project/entities/Field.java
@@ -1,5 +1,7 @@
 package com.project.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -8,7 +10,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "fields")
-public class Field {
+public class Field implements Serializable {
 	
 	/*
 			CREATE TABLE fields (
