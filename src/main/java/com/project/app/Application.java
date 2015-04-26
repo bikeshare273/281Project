5525f3557d37dd26ac42973f1dbe4d06ffcf6678
vireshjivane <viresh.jivane@sjsu.edu>
2015-04-25 21:07:19 -0700

    Commit - Post converting to Maven and Building.

--- src/main/java/com/project/app/Application.java
+++ src/main/java/com/project/app/Application.java
@@ -10,6 +10,8 @@ import com.project.controller.AppController;
 
 
 
+
+
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
--- src/main/java/com/project/controller/AppController.java
+++ src/main/java/com/project/controller/AppController.java
@@ -1,14 +1,14 @@
 package com.project.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
-@EnableAutoConfiguration
 @RestController
+@EnableAutoConfiguration
+@ComponentScan
 @RequestMapping("/api/v1/*")
-public class AppController extends WebMvcConfigurerAdapter {
-	
+public class AppController {
+
 }
